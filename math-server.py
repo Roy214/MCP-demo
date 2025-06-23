2262bd6b17d0d056b3afa5b7089bf16af2cc0fcb
Abhijit <abroy@redhat.com>
2025-06-24 01:16:54 +0530

    Adding new file

---
+++ math-server.py
@@ -0,0 +1,20 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Math")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers together"""
+    return a + b
+
+@mcp.tool()
+def subtract(a: int, b: int) -> int:
+    """Subtract two numbers"""
+    return a - b
+
+
+if __name__ == "__main__":
+    # The transport is the protocol that the server will use to communicate with the client.
+    # In this case, we are using the stdio transport, which is the default transport for the MCP server.
+    # This means that the server will communicate with the client over the standard input and output streams.   
+    mcp.run(transport="stdio")
